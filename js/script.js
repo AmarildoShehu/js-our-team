d781210ad1e1d4162fda864b3543f7037e7811da
Amarildo <amarildos@outloo.it>
2024-01-15 16:41:47 +0100

    added array object, printed on console

--- js/script.js
+++ js/script.js
@@ -18,3 +18,18 @@ Barbara Ramos |	Graphic Designer     |	barbara-ramos-graphic-designer.jpg
 5. creare layout
 
 */
+
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+const teamMembers = [
+    { name: "Wayne Barnett", role: "Founder & CEO", photo: "wayne-barnett-founder-ceo.jpg" },
+    { name: "Angela Caroll", role: "Chief Editor", photo: "angela-caroll-chief-editor.jpg" },
+    { name: "Walter Gordon", role: "Office Manager", photo: "walter-gordon-office-manager.jpg" },
+    { name: "Angela Lopez", role: "Social Media Manager", photo: "angela-lopez-social-media-manager.jpg" },
+    { name: "Scott Estrada", role: "Developer", photo: "scott-estrada-developer.jpg" },
+    { name: "Barbara Ramos", role: "Graphic Designer", photo: "barbara-ramos-graphic-designer.jpg" }
+];
+
+// Stampare su console le informazioni di nome, ruolo e la stringa della foto
+teamMembers.forEach(member => {
+    console.log(`%cNome:%c ${member.name}, %cRuolo:%c ${member.role}, %cFoto:%c ${member.photo}`, 'font-weight: bold', '', 'font-weight: bold', '', 'font-weight: bold', '');
+});
