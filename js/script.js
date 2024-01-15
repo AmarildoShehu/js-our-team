adaf91a78e4d29a6b5b0cfa2403eeaae4e0cf378
Amarildo <amarildos@outloo.it>
2024-01-15 16:35:13 +0100

    linked my ladded

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 console.log('JS OK');
+
+// MY LADDER
+
+/*
+// # DATI
+Wayne Barnett |	Founder & CEO        |	wayne-barnett-founder-ceo.jpg
+Angela Caroll |	Chief Editor         |	angela-caroll-chief-editor.jpg
+Walter Gordon |	Office Manager       |	walter-gordon-office-manager.jpg
+Angela Lopez  |	Social Media Manager |	angela-lopez-social-media-manager.jpg
+Scott Estrada |	Developer            |	scott-estrada-developer.jpg
+Barbara Ramos |	Graphic Designer     |	barbara-ramos-graphic-designer.jpg
+
+1. Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+2. Stampare su console le informazioni di nome, ruolo e la stringa della foto
+3. Stampare le stesse informazioni su DOM sottoforma di stringhe
+4. Trasformare la stringa foto in una immagine effettiva
+5. creare layout
+
+*/
