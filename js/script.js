26899e7e9d302ff7c4b2a0e02ff5efb7e2797bcf
Amarildo <amarildos@outloo.it>
2024-01-15 18:31:29 +0100

    added style to html file

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ Barbara Ramos |	Graphic Designer     |	barbara-ramos-graphic-designer.jpg
 const teamMembers = [
     { name: "Wayne Barnett", role: "Founder & CEO", photo: "img/wayne-barnett-founder-ceo.jpg" },
     { name: "Angela Caroll", role: "Chief Editor", photo: "img/angela-caroll-chief-editor.jpg" },
-    { name: "Walter Gordon", role: "Office Manager", photo: "img/imgwalter-gordon-office-manager.jpg" },
+    { name: "Walter Gordon", role: "Office Manager", photo: "img/walter-gordon-office-manager.jpg" },
     { name: "Angela Lopez", role: "Social Media Manager", photo: "img/angela-lopez-social-media-manager.jpg" },
     { name: "Scott Estrada", role: "Developer", photo: "img/scott-estrada-developer.jpg" },
     { name: "Barbara Ramos", role: "Graphic Designer", photo: "img/barbara-ramos-graphic-designer.jpg" }
@@ -31,7 +31,7 @@ const teamMembers = [
 
 // Stampare su console le informazioni di nome, ruolo e la stringa della foto
 teamMembers.forEach(member => {
-    console.log(`%cNome:%c ${member.name}, %cRuolo:%c ${member.role}, %cFoto:%c ${member.photo}`, 'font-weight: bold', '', 'font-weight: bold', '', 'font-weight: bold', '');
+    console.log(`%cNome:%c ${member.name}, %cRuolo:%c ${member.role}, ${member.photo}`, 'font-weight: bold', '', 'font-weight: bold', '', 'font-weight: bold', '');
 });
 
 
@@ -52,8 +52,8 @@ teamMembers.forEach(member => {
     imageElement.src = member.photo;
     memberElement.appendChild(imageElement);
     
-    const textElement = document.createElement('span');
-    textElement.innerHTML = `<span class="bold">Nome:</span> ${member.name}, <span class="bold">Ruolo:</span> ${member.role}, <span class="bold">Foto:</span> `;
+    const textElement = document.createElement('p');
+    textElement.innerHTML = `<h4>Nome:</h4> ${member.name}, <h4>Ruolo:</h4> ${member.role} `;
     memberElement.appendChild(textElement);
     
     teamContainer.appendChild(memberElement);
